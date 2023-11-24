49fc2553908aea44326adf5a97a5a12577b79f08
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2023-11-24 12:45:58 +0100

    Move tech stack to variable

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,34 +1,26 @@
 import "../styles/stack.scss";
 
+const techStack = [
+  "HTML",
+  "CSS",
+  "SCSS",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Next.js",
+  "Git",
+];
+
 function Stack() {
   return (
     <section className="stack">
       <h2 className="stack__heading">Stack</h2>
       <div className="stack__cards">
-        <div className="stack__card">
-          <p className="stack__name">HTML</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">CSS</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">SCSS</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">Javascript</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">Typescript</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">React</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">Next.js</p>
-        </div>
-        <div className="stack__card">
-          <p className="stack__name">Git</p>
-        </div>
+        {techStack.map((stack) => (
+          <div key={stack} className="stack__card">
+            <p className="stack__name">{stack}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
