45b21d126475124e1b5de2cea9d23e9e7f8405c5
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2024-11-22 14:56:44 +0100

    Add waving hand in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,26 @@
 import "../styles/card.scss";
 
-function Card({ title, description }: { title: string; description: string }) {
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+function Card({ title, description }: CardProps) {
+  const [baseTitle, specialTitle] = title.includes("👋")
+    ? title.split(". ")
+    : [title, null];
+
   return (
     <div className="card">
-      <h1 className="card__title">{title}</h1>
+      <h1 className="card__title">
+        {specialTitle ? (
+          <>
+            {baseTitle}. <span className="card__waving-hand">{specialTitle}</span>
+          </>
+        ) : (
+          baseTitle
+        )}
+      </h1>
       <p className="card__description">{description}</p>
     </div>
   );
