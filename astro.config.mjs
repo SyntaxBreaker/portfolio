22979f8b8669876e74e93a47cb0026ae60b900ad
SyntaxBreaker <60369185+SyntaxBreaker@users.noreply.github.com>
2026-04-29 14:13:25 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://syntaxbreaker.netlify.app",
+  integrations: [react(), sitemap()],
 });
